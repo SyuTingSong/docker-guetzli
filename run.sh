a1a1be92b39412bd45389802a72fd5cd2d948937
TingSong-Syu <rek@rek.me>
2017-05-10 16:15:19 +0800

    move converted JPEG files to dir ``converted''

--- run.sh
+++ run.sh
@@ -7,12 +7,16 @@ if [ $SUBCMD = 'all' ]; then
         echo "Creating output dir..."
         mkdir output
     fi
+    if [ ! -d converted ]; then
+        echo "Creating converted dir..."
+        mkdir converted
+    fi
     echo Quality: $QUALITY
     echo Compressing `ls *.jpg |wc -l` files with $MAX_PROCS threads...
     for f in *.jpg; do
         sem -j $MAX_PROCS "echo Start convert $f;\
             guetzli ${@:3} -quality $QUALITY \"$f\" \"output/$f\" ;\
-            echo $f converted ; mv \"$f\" \"$f.converted\""\
+            echo $f converted ; mv \"$f\" converted/"\
             2>/dev/null
     done
     sem --wait 2>/dev/null
