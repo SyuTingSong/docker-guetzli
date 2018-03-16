079102f4d90dd2daa47212e5f856ab4fc04311f5
TingSong-Syu <rek@rek.me>
2018-03-16 15:00:30 +0800

    convert png file as well.

--- one.sh
+++ one.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 f=$1
+o=${f%.*}.jpg
 LEN=${#f}
 echo Start convert $f
-guetzli ${@:$LEN} --quality $QUALITY "$f" "output/$f"
+guetzli ${@:$LEN} --quality $QUALITY "$f" "output/$o"
 if [ $? = 0 ]; then
     echo $f converted
 else
@@ -11,8 +12,8 @@ else
 fi
 mv "$f" original/
 
-if [ "$KEEP_EXIF" = "1" -a -f "output/$f" ]; then
-    exiftool -q -tagsFromFile "original/$f" "output/$f"
-    rm "output/${f}_original"
+if [ "$KEEP_EXIF" = "1" -a -f "output/$o" ]; then
+    exiftool -q -tagsFromFile "original/$f" "output/$o"
+    rm "output/${o}_original"
 fi
 
--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ if [ $SUBCMD = 'all' ]; then
         mkdir original
     fi
     echo Quality: $QUALITY
-    echo Compressing `ls *.jpg |wc -l` files with $MAX_PROCS threads...
-    for f in *.jpg; do
+    echo Compressing `ls *.jpg *.JPG *.png *.PNG |wc -l` files with $MAX_PROCS threads...
+    for f in *.jpg *.JPG *.png *.PNG; do
         sem -j $MAX_PROCS "/one.sh \"$f\" ${@:3}" 2>/dev/null
     done
     sem --wait 2>/dev/null
